aa6de418e3b27637383eed3fbe2bcb154cc585be
StyPal <126196078+StyPal@users.noreply.github.com>
2023-03-30 08:56:32 +0200

    FileHandler

---
+++ src/FileHandler.kt
@@ -0,0 +1,24 @@
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Paths
+
+class FileHandler(fileName: String?) {
+    private var fileString: String
+
+    init {
+        val wholeFile = StringBuilder()
+        try {
+            for (line in fileName?.let { Paths.get(it) }?.let { Files.readAllLines(it, StandardCharsets.ISO_8859_1) }!!) {
+                wholeFile.append(line).append("\n")
+            }
+        } catch (ioException: IOException) {
+            System.err.println("Exception: $ioException")
+        }
+        fileString = wholeFile.toString()
+    }
+
+    fun getFileString(): String{
+        return fileString
+    }
+}
